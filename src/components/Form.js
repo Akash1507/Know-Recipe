ec4e8c38c87118d05b0fc909906ff4ecc619cb31
Akash1507 <atakashsharma@gmail.com>
2020-01-07 17:27:14 +0530

    create a form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Form = props => (
-  <form onSubmit={props.getRecipe}>
-    <input type="text" name="query" />
-    <button>Search</button>
+  <form onSubmit={props.getRecipe} styles={{ marginBottom: "2rem" }}>
+    <input className="form__input" type="text" name="query" />
+    <button className="form__button">Search</button>
   </form>
 );
 
